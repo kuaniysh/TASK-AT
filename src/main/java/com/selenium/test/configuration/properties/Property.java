0ca25745cd14371d7814d85b83f65729111b371a
kuaniysh <kuaniysh@gmail.com>
2016-12-25 16:34:43 +0600

    Delete Author

--- src/main/java/com/selenium/test/configuration/properties/Property.java
+++ src/main/java/com/selenium/test/configuration/properties/Property.java
@@ -6,7 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Created by Kuanysh Bekturganov
  * Annotates field of {@link com.selenium.test.configuration.TestsConfig} fields for setting to this field
  * value from properties - system or file(about file you could find information in {@link com.selenium.test.configuration.properties.PropertyFile} class javadoc
  */
